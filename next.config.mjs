96d539a993c4b71c977a3563ebe1c2a1815a7699
Dhaafin <dhaafinm@gmail.com>
2026-04-24 23:52:18 +0700

    feat : setting for github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/code-share', // Remove if using a custom domain or root site
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
