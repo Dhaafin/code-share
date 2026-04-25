581ac4af76c1df8bac80db6b39f1ab4073a849c2
Dhaafin <dhaafinm@gmail.com>
2026-04-25 21:14:05 +0700

    fix : remove next config for vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/code-share', // Remove if using a custom domain or root site
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
